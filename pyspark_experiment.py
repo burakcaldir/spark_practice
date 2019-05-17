6c0e255d005d66b975ac3e5407355b6761f8e5e5
Stephanie <s.kirmer@gmail.com>
2019-05-17 15:29:51 -0500

    linted

--- pyspark_experiment.py
+++ pyspark_experiment.py
@@ -6,30 +6,31 @@ from pyspark.sql import SparkSession
 import feather
 
 # Set up environment and start spark session
-conf = pyspark.SparkConf().set('spark.driver.host','127.0.0.1')
-sc = pyspark.SparkContext(master='local', appName='myAppName',conf=conf)
+conf = pyspark.SparkConf().set("spark.driver.host", "127.0.0.1")
+sc = pyspark.SparkContext(master="local", appName="myAppName", conf=conf)
 sqlContext = SQLContext(sc)
 
 #######################
-sparksesh = SparkSession \
-    .builder \
-    .appName("Python Spark SQL basic example") \
-    .config("spark.some.config.option", "some-value") \
+sparksesh = (
+    SparkSession.builder.appName("Python Spark SQL basic example")
+    .config("spark.some.config.option", "some-value")
     .getOrCreate()
+)
 
 ######################
 # Call data from csv quick and dirty
 
 def make_spark_data(selection, filepath_csv):
-    if(selection == "pyspark"):
+    if selection == "pyspark":
         # This makes a spark dataframe
-        exp = sparksesh.read.option("header", "true").csv(filepath_csv) 
-    elif(selection == "RDD"): 
+        exp = sparksesh.read.option("header", "true").csv(filepath_csv)
+    elif selection == "RDD":
         # This makes an RDD
         exp = sc.textFile(filepath_csv)
-    return(exp)
+    return exp
 
-#sp_data = make_spark_data(selection = "pyspark", filepath_csv = "")
+
+# sp_data = make_spark_data(selection = "pyspark", filepath_csv = "")
 
 ######################
 # Load data from a feather file (just spark df)
@@ -46,10 +47,10 @@ print(sp_data.schema.names)
 ###################
 # These tasks work on RDD and on spark DF
 
-print(sp_data.count()) #number of rows
-print(sp_data.first()) # first row
+print(sp_data.count())  # number of rows
+print(sp_data.first())  # first row
 
-# Other attributes: distinct, filter, foreach, groupBy, histogram, join, max, 
+# Other attributes: distinct, filter, foreach, groupBy, histogram, join, max,
 # mean, min, name, randomSplit, reduce, sample, saveAsPickleFile, saveAsTextFile,
 # sortBy, stats, stdev, subtract (removing records), sum, take (like head), top
 
@@ -71,7 +72,9 @@ print(no_na_sp.select(["p25_earnings", "p50_earnings"]).show())
 
 # Establish as a table, query it
 sp_data.registerTempTable("ut_grads")
-query1 = sqlContext.sql("select * from ut_grads where ut_grads.cellcount > 0 limit 3 ").show()
+query1 = sqlContext.sql(
+    "select * from ut_grads where ut_grads.cellcount > 0 limit 3 "
+).show()
 print(query1)
 
 #########################
@@ -79,13 +82,15 @@ print(query1)
 
 sp_data.registerTempTable("ut_grads")
 
-query2 = sqlContext.sql("select \
+query2 = sqlContext.sql(
+    "select \
     mean(p50_earnings) as earn, \
     year_postgrad \
     from ut_grads \
     where ut_grads.cellcount > 0 \
     and ut_grads.deglevl_code = 3 \
-    group by ut_grads.year_postgrad")
+    group by ut_grads.year_postgrad"
+)
 
 query2.show()
 print(type(query2))
@@ -94,5 +99,5 @@ print(query2)
 import matplotlib.pyplot as plt, numpy as np
 
 utgrad_df = query2.toPandas()
-utgrad_df.plot.bar(x='year_postgrad', y='earn', rot=0)
+utgrad_df.plot.bar(x="year_postgrad", y="earn", rot=0)
 plt.show()
